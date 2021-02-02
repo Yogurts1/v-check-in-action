f4e3b0be3c4421e6277d2291538a9e307e032682
cy920820 <cuiyang673308817@gmail.com>
2021-02-02 14:30:42 +0800

    fix(main.py): fixed cookie invalidation

--- main.py
+++ main.py
@@ -3,22 +3,25 @@ import os
 
 # 参考这个网站https://sc.ftqq.com/获取自己的sckey，如果cookie失效会通过server酱提示
 sckey = os.environ["SERVERCHAN_SCKEY"]
-Cookie = os.environ["V_COOKIE"]
 send_url = "https://sc.ftqq.com/%s.send" % (sckey)
-# https://access.video.qq.com/user/auth_refresh
+
+# https://access.video.qq.com/user/auth_refresh 获取 cookie
+login_cookie = os.environ["V_COOKIE_LOGIN"]
+signin_cookie = os.environ["V_COOKIE_SIGNIN"]
+auth_refresh_url = os.environ["V_AUTH_REFRESH_URL"]
 Referer = 'https://v.qq.com'
 Agent = 'User-Agent: Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1'
-headers = {
-    'User-Agent': Agent,
-    'Cookie': Cookie,
-    'Referer': Referer
+headers_login = {
+  'User-Agent': Agent,
+  'Cookie': login_cookie,
+  'Referer': Referer
 }
 
 # 测试一个签到请求
-login = requests.get(
-    'https://vip.video.qq.com/fcgi-bin/comm_cgi?name=spp_MissionFaHuo&cmd=4&task_id=3&_=1582368319252&callback=Zepto1582368297765 ', headers=headers)
+login = requests.get(auth_refresh_url, headers=headers_login)
+cookie = requests.utils.dict_from_cookiejar(login.cookies)
 # 如果请求返回信息包含no login说明cookie已经失效
-if login.content.decode("utf-8").__contains__("no login"):
+if not cookie:
     print("auth_refresh error")
     params = {'text': '腾讯视频V力值签到通知', 'desp': '获取Cookie失败，Cookie失效'}
     requests.post(send_url, params=params)
@@ -48,7 +51,13 @@ for url in urls:
         print("发送每日弹幕任务请求")
     elif (count == 5):
         print("发送每日观影60分钟任务请求")
-    response = requests.get(url=url, headers=headers)
+    refresh_cookie = cookie['vusession']
+    headers_signin = {
+      'User-Agent': Agent,
+      'Cookie': signin_cookie + refresh_cookie + ';_video_qq_vusession=' + refresh_cookie + ';',
+      'Referer': Referer
+    }
+    response = requests.get(url=url, headers=headers_signin)
     responseContent = response.content.decode("utf-8")
     print(responseContent)
     resultContent += responseContent + '\n\n'
